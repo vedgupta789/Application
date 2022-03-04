031db78b4e96da78706405d72f6ece7843401ed3
Ved Gupta <vedgupta789@gmail.com>
2022-03-04 19:24:02 +0530

    Hand detection done, responsiveness done at basic level

--- App.js
+++ App.js
@@ -1,104 +1,24 @@
-// let video = document.getElementById("video");
-// // let model;
-// let canvas = document.getElementById("canvas");
-// let ctx = canvas.getContext("2d");
-
-// const setupCamera = () => {
-//     navigator.mediaDevices.getUserMedia({
-//         video : {width:600, height:400},
-//         audio : false,
-//     })
-//     .then((stream) => {
-//         video.srcObject = stream;
-//     });
-// };
-
-
-
-// const detectHands = async () => {
-//     const pred = await model.estimateFaces(video, false);
-//     // const model = await handpose.load();
-//     // const hands = await model.estimateHands(video);
-//     ctx.drawImage(video, 0, 0, 600, 400);
-//     pred.forEach(pre => {
-//         ctx.beginPath();
-//         ctx.lineWidth = "4";
-//         ctx.strokeStyle = "blue";
-//         ctx.rect(
-//             pre.topLeft[0],
-//             pre.topLeft[1],
-//             pre.bottomRight[0] - pre.topLeft[0],
-//             pre.bottomRight[1] - pre.topLeft[1]
-//         );
-//         ctx.stroke();
-//         ctx.fillStyle="red";
-//         pre.landmarks.forEach((landmark) => {
-//             ctx.fillRect(landmark[0], landmark[1], 5,5);
-//         });
-//     });
-// };
-
-// const detectFaces = async () => {
-//     const pred = await model.estimateFaces(video, false);
-//     // const model = await handpose.load();
-//     // const hands = await model.estimateHands(video);
-//     ctx.drawImage(video, 0, 0, 600, 400);
-//     pred.forEach(pre => {
-//         ctx.beginPath();
-//         ctx.lineWidth = "4";
-//         ctx.strokeStyle = "blue";
-//         ctx.rect(
-//             pre.topLeft[0],
-//             pre.topLeft[1],
-//             pre.bottomRight[0] - pre.topLeft[0],
-//             pre.bottomRight[1] - pre.topLeft[1]
-//         );
-//         ctx.stroke();
-//         ctx.fillStyle="red";
-//         pre.landmarks.forEach((landmark) => {
-//             ctx.fillRect(landmark[0], landmark[1], 5,5);
-//         });
-//     });
-// };
-
-// setupCamera();
-// video.addEventListener("loadeddata", async () =>{
-//     model = await blazeface.load();
-//     setInterval(detectFaces, 100);
-// });
-
-// video.addEventListener("loadeddata", async () =>{
-//     const model = handPoseDetection.SupportedModels.MediaPipeHands;
-//     const detectorConfig = {
-//         runtime: 'mediapipe', // or 'tfjs',
-//         solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/hands',
-//         modelType: 'full'
-//       }
-    
-//     const detector = await handPoseDetection.createDetector(model, detectorConfig);
-//     const hands =  await detector.estimateHands(image);
-//     console.log(hands);
-//     // setInterval(detectFaces, 100);
-// });
-    
-<script type="module">
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const outputText = document.getElementsByClassName('output_text')[0];
+const loader = document.getElementById('loader');
 const canvasCtx = canvasElement.getContext('2d');
 
 function onResults(results) {
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
-  if (results.multiHandLandmarks) {
-    for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-                     {color: '#00FF00', lineWidth: 5});
-      drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+    loader.style.display="none";
+    canvasElement.style.display="block";
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+    if (results.multiHandLandmarks) {
+        for (const landmarks of results.multiHandLandmarks) {
+        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+                        {color: '#FFFFFF', lineWidth: 5});
+        drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 5});
+        }
     }
-  }
-  canvasCtx.restore();
+    canvasCtx.restore();
 }
 
 const hands = new Hands({locateFile: (file) => {
@@ -111,7 +31,6 @@ hands.setOptions({
   minTrackingConfidence: 0.5
 });
 hands.onResults(onResults);
-
 const camera = new Camera(videoElement, {
   onFrame: async () => {
     await hands.send({image: videoElement});
@@ -120,4 +39,3 @@ const camera = new Camera(videoElement, {
   height: 720
 });
 camera.start();
-</script>
